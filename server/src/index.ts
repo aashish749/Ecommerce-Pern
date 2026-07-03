3f260fb5a462d95c862ac41bbb7c06d4e49fa7a4
Aashish Chalise <aashishchalise01@gmail.com>
2026-07-03 23:40:20 +0100

    fixed clerk md2

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "http://localhost:5176",
+      "https://ecommerce-pern.aashis.dev",
       "https://ecommerce-pern-client-three.vercel.app",
       process.env.CLIENT_URL,
     ].filter((x): x is string => Boolean(x)),
